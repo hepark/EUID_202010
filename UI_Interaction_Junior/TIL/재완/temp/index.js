c039254e1c0c7f6c8081ae18a22a5075ebd05f59
지훈 <yamoog@naver.com>
2020-10-17 21:41:15 +0900

    [리팩토링] getNode에서 사용할 유틸리티 함수 throwError 함수 추가

--- UI_Interaction_Junior/TIL/재완/temp/index.js
+++ UI_Interaction_Junior/TIL/재완/temp/index.js
@@ -1,6 +1,10 @@
+function throwError(message) {
+  throw new Error(message);
+}
+
 function getNode(selector, context = document) {
   if (typeof selector !== 'string') {
-    throw new Error('첫번째 전달인자는 문자 유형이어야 합니다.');
+    throwError('첫번째 전달인자는 문자 유형이어야 합니다.');
   }
   return context.querySelector(selector);
 }
