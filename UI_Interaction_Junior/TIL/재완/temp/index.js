510866f6f8bdc8b559cd5fc9d2174037048790aa
지훈 <yamoog@naver.com>
2020-10-17 21:40:16 +0900

    [리팩토링] getNode 전달인자 조건 처리

--- UI_Interaction_Junior/TIL/재완/temp/index.js
+++ UI_Interaction_Junior/TIL/재완/temp/index.js
@@ -1,3 +1,6 @@
 function getNode(selector, context = document) {
+  if (typeof selector !== 'string') {
+    throw new Error('첫번째 전달인자는 문자 유형이어야 합니다.');
+  }
   return context.querySelector(selector);
 }
