b3a7693bb2c9f467d078af4d1cc42994bcc6b544
unknown <shhn0509@gmail.com>
2020-11-07 18:44:20 +0900

    [오타 정정] `ey` -> `ye`

--- UI_Interaction_Junior/lecture/week01/project__(WS)webScript--begin/js/guide.js
+++ UI_Interaction_Junior/lecture/week01/project__(WS)webScript--begin/js/guide.js
@@ -19,7 +19,7 @@
 /**
  * * [ 함수 미션 ]
  *
- * * 미션 1-1. 평방미터(㎡, square meter) → 평(坪, peyong) 함수를 작성해보세요. (참고: 3.3058㎡ = 1평)
+ * * 미션 1-1. 평방미터(㎡, square meter) → 평(坪, pyeong) 함수를 작성해보세요. (참고: 3.3058㎡ = 1평)
  * * 미션 1-2. 작성한 함수를 사용해 26평(坪)이 몇 평방미터(㎡)인지 계산해보세요.
  *
  * * 미션 2-1. 평(坪) → 평방미터(㎡) 함수를 작성해보세요.
@@ -34,17 +34,17 @@
 const SQUARE_METERS_PER_PYEONG = 3.3058;
 
 //[미션 1-1, 1-2] 평방미터 값을 평수 값으로 반환하는 유틸리티 함수
-function squareMeter2peyong(squareMeterValue) {
+function squareMeter2pyeong(squareMeterValue) {
   return squareMeterValue / SQUARE_METERS_PER_PYEONG;
 }
 
 // [미션 2-1, 2-2] 평수 값을 평방미터 값으로 반환하는 유틸리티 함수
-function peyong2squareMeter(peyongValue) {
-  return peyongValue * SQUARE_METERS_PER_PYEONG;
+function pyeong2squareMeter(pyeongValue) {
+  return pyeongValue * SQUARE_METERS_PER_PYEONG;
 }
 
-var result1 = squareMeter2peyong(158.6784); // 몇 평 ???
-var result2 = peyong2squareMeter(result1);
+var result1 = squareMeter2pyeong(158.6784); // 몇 평 ???
+var result2 = pyeong2squareMeter(result1);
 
 console.log('result1:', result1); // 48 평
 console.log('result2:', result2); // 158.6784 평방미터
@@ -52,19 +52,19 @@ console.log('result2:', result2); // 158.6784 평방미터
 /* -------------------------------------------------------------------------- */
 
 // 미션 3-1. 평방미터(㎡) ↔ 평(坪) 함수를 작성해보세요. (조건에 따라 전달된 값을 평(坪) 또는 평방미터(㎡)로 반환)
-function toggleFloorSpaceValue(value, isPeyongValue) {
+function toggleFloorSpaceValue(value, ispyeongValue) {
   // 프로그래밍 사고의 힘(조건에 따른 변수를 만들 수 있어야 한다)
   // "값의 유형이 '평'이니?"
-  // if (isPeyongValue) {
+  // if (ispyeongValue) {
   //   // 조건 1. 값이 평수인 경우
-  //   return peyong2squareMeter(value);
+  //   return pyeong2squareMeter(value);
   // } else {
   //   // 조건 2. 값이 평방미터인 경우
-  //   return squareMeter2peyong(value);
+  //   return squareMeter2pyeong(value);
   // }
 
   // 3항식
-  return isPeyongValue ? peyong2squareMeter(value) : squareMeter2peyong(value);
+  return ispyeongValue ? pyeong2squareMeter(value) : squareMeter2pyeong(value);
 }
 
 // 미션 3-2. 작성한 함수를 사용해 32평(坪) 값을 전달해 평방미터 값을 구해보세요.
