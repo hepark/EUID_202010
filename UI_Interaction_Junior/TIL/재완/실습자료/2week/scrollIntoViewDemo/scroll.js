99035d24855d2a32acc4e78c16626c7fbc0f9fdc
Shane-shim <shane@rebeltech.info>
2020-11-09 13:40:24 +0900

    [재완프로젝트]스크롤

---
+++ UI_Interaction_Junior/TIL/재완/실습자료/2week/scrollIntoViewDemo/scroll.js
@@ -0,0 +1,13 @@
+var scene_list = document.querySelectorAll(".scrollView__scene"); // 이미지3개
+var tab_list = document.querySelectorAll(".scrollView__controlTab"); // 조작할 버튼 3개
+// 매서드 빌려쓰기 패턴을 활용해서 코드 리펙토링
+var handle = function (index) {
+  return function (e) {
+    e.preventDefault();
+    scene_list[index].scrollIntoView();
+  };
+};
+Array.from(tab_list).forEach(function (item, index) {
+  //인터넷 익스플로러에서 작동안됨.
+  item.addEventListener("click", handle(index));
+});
