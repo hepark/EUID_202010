d274e72a8475764b01e256ba92cc1bd1f8f43a68
Shane-shim <shane@rebeltech.info>
2020-11-09 13:30:30 +0900

    [코드리펙토링]

--- UI_Interaction_Junior/lecture/week02/scrollIntoViewDemo/scroll.js
+++ UI_Interaction_Junior/lecture/week02/scrollIntoViewDemo/scroll.js
@@ -1,20 +1,13 @@
-var img_list = document.querySelectorAll(".scrollView__scene"); // 이미지3개
-var el_list = document.querySelectorAll(".scrollView__controlTab"); // 조작할 버튼 3개
-
-el_list.forEach(function (item, index) {
-  item.addEventListener("click", function (e) {
+var scene_list = document.querySelectorAll(".scrollView__scene"); // 이미지3개
+var tab_list = document.querySelectorAll(".scrollView__controlTab"); // 조작할 버튼 3개
+// 매서드 빌려쓰기 패턴을 활용해서 코드 리펙토링
+var handle = function (index) {
+  return function (e) {
     e.preventDefault();
-    console.log(index, item);
-    switch (index) {
-      case 0:
-        img_list[0].scrollIntoView();
-        break;
-      case 1:
-        img_list[1].scrollIntoView();
-        break;
-      case 2:
-        img_list[2].scrollIntoView();
-        break;
-    }
-  });
+    scene_list[index].scrollIntoView();
+  };
+};
+Array.from(tab_list).forEach(function (item, index) {
+  //인터넷 익스플로러에서 작동안됨.
+  item.addEventListener("click", handle(index));
 });
