22832a2a7eaaf9b3707377a0e633e636d96cf80e
지훈 <yamoog@naver.com>
2020-11-21 17:46:38 +0900

    [실전] 커리 함수의 ES5 버전

---
+++ UI_Interaction_Junior/lecture/week04/practice/curring.js
@@ -0,0 +1,24 @@
+// 배열 → 복사(원본 보호) → 정렬 → 새로운 배열 반환
+function sortArray(compareFn, array) {
+  return array.slice().sort(compareFn)
+}
+
+function currySortArray(compareFn) {
+  return function (array) {
+    return array.slice().sort(compareFn)
+  }
+}
+
+const orgArray = [3, 2, 7, 10, -11]
+
+// 오름차순 정렬
+sortArray((z, k) => z - k, orgArray)
+
+// 내림차순 정렬
+sortArray((z, k) => k - z, orgArray)
+
+const ascSortArray = currySortArray((z, k) => z - k)
+const descSortArray = currySortArray((z, k) => k - z)
+
+ascSortArray(orgArray) // 오름차순 정렬
+descSortArray(orgArray) // 내림차순 정렬
